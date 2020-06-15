c8abcc8353bf01b7e055ed8b27e213f9d7b9ab5c
gr8geek <pratyush9000@gmail.com>
2020-06-16 00:45:14 +0530

    First Flat list

--- App.js
+++ App.js
@@ -1,19 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text,SectionList,StyleSheet,FlatList, View} from 'react-native';
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+};
+export default App = ()=>{
+      return(
+        <View>
+                 
+                 <FlatList
+          style={{
+            backgroundColor:"#ad0a67",
+          }}
+          data={
+            [
+              {key: 'Devin'},
+              {key: 'Dan'},
+              {key: 'Dominic'},
+              {key: 'Jackson'},
+              {key: 'James'},
+              {key: 'Joel'},
+              {key: 'John'},
+              {key: 'Jillian'},
+              {key: 'Jimmy'},
+              {key: 'Julie'},
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+            ]
+          }
+          renderItem={({item})=>{
+            return(
+              <Text style={{
+                padding: 40,
+                fontSize: 30,
+                borderWidth: 7,
+                borderColor:"#85d11b",
+                backgroundColor:"#7766ee",
+                borderRadius:15,
+              }}>{item.key}</Text>
+            )
+          }}>
+            
+          </FlatList>
+          <SectionList
+          sections={[
+            {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      
+        </View>
+        
+      )
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
   },
-});
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
